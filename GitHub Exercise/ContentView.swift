bd274db8ee3f4b3b822e421cf8a2a18fc2672a66
Likhith Mele Areekkal <kubera9200@gmail.com>
2023-08-21 15:20:34 -0400

    development - added rectangle

--- GitHub Exercise/ContentView.swift
+++ GitHub Exercise/ContentView.swift
@@ -16,6 +16,7 @@ struct ContentView: View {
             Text("Ver 2.0")
                 .font(.largeTitle)
                 .fontWeight(.black)
+            Rectangle()
         }
         .padding()
         .foregroundColor(.green)
